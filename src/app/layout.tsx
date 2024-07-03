0f4dda6b170b29eefcc116d1b63f8efb00798381
Ian Nerney <inerney@webstaurantstore.com>
2024-07-03 08:32:37 -0400

    Add AntDesign and Prettier, update ESLint configs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { AntdRegistry } from '@ant-design/nextjs-registry';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}><AntdRegistry>{children}</AntdRegistry></body>
     </html>
   );
 }
