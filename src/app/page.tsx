ac9a75cea785a9bdb9d4642cf05ae5be6b96ce22
Ian Nerney <inerney@webstaurantstore.com>
2024-07-11 10:14:46 -0400

    Fix padding/gutter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ const Home: React.FC = () => {
     const [customizableTheme, setCustomizableTheme] = useState<ThemeConfig>(theme.defaultConfig);
     return (
         <AntDesignLayout>
-            <Row gutter={64}>
+            <Row style={{ padding: "24px 26px" }}>
                 <Col style={{ padding: "24px" }} xs={24} md={12}>
                     <CustomizeThemeSection customizableTheme={customizableTheme} setCustomizableTheme={setCustomizableTheme} />
                 </Col>
