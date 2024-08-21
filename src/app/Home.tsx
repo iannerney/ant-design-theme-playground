571572614d9541d86c8934ae85c840be5b83f90e
Ian Nerney <inerney@webstaurantstore.com>
2024-08-21 10:13:03 -0400

    Padding fix

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -10,7 +10,7 @@ const Home: React.FC = () => {
 
     return (
         <AntDesignLayout>
-            <Row>
+            <Row className="home__row">
                 <Col style={{ padding: "24px" }} xs={24} md={12}>
                     <CustomizeThemeSection
                         customizableTheme={customizableTheme}
