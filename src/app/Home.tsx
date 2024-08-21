9aaa531a6d3deb1308db1ada653b4bf22fbec5c0
Ian Nerney <inerney@webstaurantstore.com>
2024-08-21 08:40:54 -0400

    Mobile responsive overrides

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -10,7 +10,7 @@ const Home: React.FC = () => {
 
     return (
         <AntDesignLayout>
-            <Row style={{ padding: "24px 26px" }}>
+            <Row>
                 <Col style={{ padding: "24px" }} xs={24} md={12}>
                     <CustomizeThemeSection
                         customizableTheme={customizableTheme}
